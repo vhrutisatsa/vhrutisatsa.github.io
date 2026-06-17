53cba45039fcb5748ea4670adf695284aa42d087
vhrutisatsa <shruti.vatsa.15@gmail.com>
2026-06-17 18:03:24 +0000

    Fix essay route order

--- artifacts/portfolio/src/pages/EssayInformationForaging.tsx
+++ artifacts/portfolio/src/pages/EssayInformationForaging.tsx
@@ -1,15 +1,94 @@
 export function EssayInformationForaging() {
   return (
     <div className="max-w-3xl mx-auto px-6 py-24">
-      <h1 className="text-5xl font-serif mb-8">
-        Foraging Theory and the Mind
-      </h1>
+      <h1 className="text-5xl font-serif mb-8">Foraging Theory and the Mind</h1>
 
       <div className="prose prose-lg max-w-none">
         <p>
-          One of the most important aspects for the survival of animals in the environment is understanding the right time and way to look for and collect food, and structure plays a very crucial role in this. The migratory birds, when the season changes, move from a place of declining resources to a place of richer resources. When you are in need of some coffee in the afternoon, the knowledge and understanding of the opening and closing time of your favourite coffee shop and its location makes the process of re-energizing yourself and working effciently more easier. These behaviours explain the understanding of the organisms about the best conditions for their most effcient survival and the temporal and environmental structure playing a key role in driving the resulting action. Hence, the act of looking for and collecting food, i.e., foraging, is one of the most important activities for all living organisms to survive, and structure of the environment plays a crucial role in modulating the foraging behaviour of organisms. As it is the most basic need of an animal to gain energy as required and maintain an energy budget for other daily life activities, their brain has evolved in a way to optimise their foraging behaviour in the best possible way. While several existing studies have looked at foraging behaviour, very few have, if at all, focused on the influence of temporal structure on it. In this thesis, I intend to focus on that aspect of foraging behaviour.<br>While foraging in the environment is one thing, the optimal way of doing that is another. All the beings of the universe always try to maximise their gain from the environment by utilising the least amount of energy possible. There are many existing theories that talk about the ways animals behave in order to maximise their utility from the environment.<br>One of the ways to maximise the utility from the environment is the optimal foraging theory, which is modelled to explain foraging behaviours of animals in an environment where food is present in patches, and a forager has to expend time to cover the distance from one patch to another. The theory is based on a few assumptions, the most important ones include: 1) the behaviour of the forager is so as to maximise their net energy gain per unit time, and 2) the rate of food intake in a patch decreases as an animal spends time there foraging.<br> One of the theorems of the optimal foraging theory is the marginal value theorem (MVT), which states that an animal will stay in a particular environment only up to a point when current intake rate in a patch becomes greater than or equal to the average intake rate in that environment.<br> This theorem formulates the foraging behaviour of animals in an environment where the resources to be foraged are present in patches throughout the environment. These patches can be accessed only sequentially, one at a time. Along with the energy cost, there’s a time cost that the forager has to incur, in the form of the travel time (to cover these interpatch distances) and stay duration (time for which they stay in a patch to forage). The reward values of the patches differ. While foraging in a patch, the resources and thus the rate of reward return of that patch diminishes continuously. The optimal energy intake rate for the environment is calculated by using the reward value as well as the interpatch travel time and stay duration. The optimal behaviour of an animal has been defined such that they leave a patch as soon as their intake rate in that patch falls below the global average intake rate in the environment. While the cost and benefit has been considered while theorising this, nothing has been talked about the effect that the environment structure might have on the behaviour. Whether a participant’s foraging behaviour who encountered rich patches only before coming to the current patch will be different from someone who has only encountered poor patches in the previous trials has not been considered while studying the behaviour, hence, ignoring one of the most important variables defining the natural environment and correspondingly the foraging behaviour.<br> We saw that environmental and/or temporal structure substantially influences our behaviours like foraging. However, studies based on understanding the foraging behaviour in animals have not explicitly factored in “structure”. Theories, like optimal foraging theory, do not account for it either. There are a few previous studies that talk about the effect of structure on foraging behaviour through different experimental paradigms while capturing certain aspects of the temporal structure of the foraging environment. Key findings from these studies indicate that the participants do infer and take advantage of structure in the environment in patch foraging, which clearly motivates the need to study this in greater detail.
-
-          
+          One of the most important aspects for the survival of animals in the
+          environment is understanding the right time and way to look for and
+          collect food, and structure plays a very crucial role in this. The
+          migratory birds, when the season changes, move from a place of
+          declining resources to a place of richer resources. When you are in
+          need of some coffee in the afternoon, the knowledge and understanding
+          of the opening and closing time of your favourite coffee shop and its
+          location makes the process of re-energizing yourself and working
+          effciently more easier. These behaviours explain the understanding of
+          the organisms about the best conditions for their most effcient
+          survival and the temporal and environmental structure playing a key
+          role in driving the resulting action. Hence, the act of looking for
+          and collecting food, i.e., foraging, is one of the most important
+          activities for all living organisms to survive, and structure of the
+          environment plays a crucial role in modulating the foraging behaviour
+          of organisms. As it is the most basic need of an animal to gain energy
+          as required and maintain an energy budget for other daily life
+          activities, their brain has evolved in a way to optimise their
+          foraging behaviour in the best possible way. While several existing
+          studies have looked at foraging behaviour, very few have, if at all,
+          focused on the influence of temporal structure on it. In this thesis,
+          I intend to focus on that aspect of foraging behaviour.
+        </p>
+        <p>
+          While foraging in the environment is one thing, the optimal way of
+          doing that is another. All the beings of the universe always try to
+          maximise their gain from the environment by utilising the least amount
+          of energy possible. There are many existing theories that talk about
+          the ways animals behave in order to maximise their utility from the
+          environment.
+        </p>
+        <p>
+          One of the ways to maximise the utility from the environment is the
+          optimal foraging theory, which is modelled to explain foraging
+          behaviours of animals in an environment where food is present in
+          patches, and a forager has to expend time to cover the distance from
+          one patch to another. The theory is based on a few assumptions, the
+          most important ones include: 1) the behaviour of the forager is so as
+          to maximise their net energy gain per unit time, and 2) the rate of
+          food intake in a patch decreases as an animal spends time there
+          foraging.
+        </p>
+        One of the theorems of the optimal foraging theory is the marginal value
+        theorem (MVT), which states that an animal will stay in a particular
+        environment only up to a point when current intake rate in a patch
+        becomes greater than or equal to the average intake rate in that
+        environment.
+        <p>
+          This theorem formulates the foraging behaviour of animals in an
+          environment where the resources to be foraged are present in patches
+          throughout the environment. These patches can be accessed only
+          sequentially, one at a time. Along with the energy cost, there’s a
+          time cost that the forager has to incur, in the form of the travel
+          time (to cover these interpatch distances) and stay duration (time for
+          which they stay in a patch to forage). The reward values of the
+          patches differ. While foraging in a patch, the resources and thus the
+          rate of reward return of that patch diminishes continuously. The
+          optimal energy intake rate for the environment is calculated by using
+          the reward value as well as the interpatch travel time and stay
+          duration. The optimal behaviour of an animal has been defined such
+          that they leave a patch as soon as their intake rate in that patch
+          falls below the global average intake rate in the environment. While
+          the cost and benefit has been considered while theorising this,
+          nothing has been talked about the effect that the environment
+          structure might have on the behaviour. Whether a participant’s
+          foraging behaviour who encountered rich patches only before coming to
+          the current patch will be different from someone who has only
+          encountered poor patches in the previous trials has not been
+          considered while studying the behaviour, hence, ignoring one of the
+          most important variables defining the natural environment and
+          correspondingly the foraging behaviour.
+        </p>
+        <p>
+          We saw that environmental and/or temporal structure substantially
+          influences our behaviours like foraging. However, studies based on
+          understanding the foraging behaviour in animals have not explicitly
+          factored in “structure”. Theories, like optimal foraging theory, do
+          not account for it either. There are a few previous studies that talk
+          about the effect of structure on foraging behaviour through different
+          experimental paradigms while capturing certain aspects of the temporal
+          structure of the foraging environment. Key findings from these studies
+          indicate that the participants do infer and take advantage of
+          structure in the environment in patch foraging, which clearly
+          motivates the need to study this in greater detail.
         </p>
       </div>
     </div>
